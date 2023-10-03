2cc36b85cfd8d4543ea662ba878b0914e30d15e4
Irish Rixon Dela Pena <irishrixon@gmail.com>
2023-10-03 20:51:09 +0800

    error message debugged

--- Login(days-algo).js
+++ Login(days-algo).js
@@ -1,41 +1,90 @@
 // MONTH CONTAINER AND ITS CHILDREN
 const monthsContainer = document.querySelector(".months_container"),
-month = monthsContainer.querySelector("#month_input"),
-errorForMonth = monthsContainer.querySelector("#error_for_month");
+month = document.querySelector("#month_input"),
+monthDatalist = document.querySelector("#months"),
+children = monthDatalist.children;
+errorForMonth = document.querySelector("#error_for_month");
 
 const day = document.querySelector("#dayslist");
-const children = month.children;
 const lengthOfChildren = children.length;
 
-let MonthValue = -1;
-function valueOfMonth() {
-    let monthInputLowercase;
-    let datalistLowercaseValue;
 
+month.addEventListener('change', function () {
+    let monthValue = -1;
+    let monthInputLowercase = "";
+    let datalistLowercaseValue = "";
+
+    // CHECK IF THE INPUTTED MONTH IS VALID
     for(let i = 0; i < lengthOfChildren; i++) {
-        datalistLowercaseValue = children[i].textContent.toLowerCase();
+        datalistLowercaseValue = children[i].innerText.toLowerCase();
         monthInputLowercase = month.value.toLowerCase();
 
         if(monthInputLowercase !== datalistLowercaseValue) {
-            errorForMonth.textContent = "Please enter a valid month";
-            return;
+            errorForMonth.innerText = "Please enter a valid month";
+            console.log("hello");
         }
         else {
-            errorForMonth.textContent = "";
+            // ELSE JUST REMOVE THE ERROR MESSAGE
+            errorForMonth.innerText = "";
+            // THE INPUTTED VALUE IS VALID AND EQUALS TO FEBRUARY REMOVE THE ERROR
+            // MESSAGE AND EXIT THE FUNCTION
+            if(monthInputLowercase === "february") {
+                monthValue = 1;
+                errorForMonth.innerText = "";
+                return;
+            }
+            else {
+                // IF THE INPUTTED MONTH IS VALID FIND THE VALUE MONTH BASE ON SEQUENCE
+                for(let i = 0; i < lengthOfChildren; i++) {
+                    if(monthInputLowercase !== datalistLowercaseValue) {
+                        monthValue++;
+                    }
+                    else {
+                        daysAlgo(monthValue);
+                        break;
+                    }
+                }
+            }
+            
         }
     }
+});
 
-    for(let i = 0; i < lengthOfChildren; i++) {
-        if(monthInputLowercase !== datalistLowercaseValue) {
-            MonthValue++;
+
+function daysAlgo(monthValue) {
+    const lastChild = day.lastElementChild,
+    numberOfChildren = day.childElementCount,
+    option = document.createElement("option");
+
+    if(monthValue === 1) {
+        while (numberOfChildren > 27) {
+            lastChild.remove();
+            numberOfChildren--;
         }
-        else {
-            break;
+    }
+    else if(monthValue % 2 == 0) {
+        if(numberOfChildren <= 27) {
+            while(numberOfChildren != 30) {
+                numberOfChildren++;
+                option.value = numberOfChildren;
+                option.innerText = numberOfChildren;
+
+                day.appendChild(option);
+            }
+        }
+    }
+    else {
+        if(numberOfChildren <= 27) {
+            while(numberOfChildren != 29) {
+                numberOfChildren++;
+                option.value = numberOfChildren;
+                option.innerText = numberOfChildren;
+
+                day.appendChild(option);
+            }
         }
     }
 }
 
-function daysAlgo() {
-    
-}
+
 
