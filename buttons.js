0c67f6325a1dad02610821563340b589cf573711
Irish Rixon Dela Pena <irishrixon@gmail.com>
2023-10-13 14:24:12 +0800

    Account Successfully Created done

--- buttons.js
+++ buttons.js
@@ -1,21 +1,23 @@
-const formPages = ["Log in", "Sign Up form 1", "Sign Up form 2", "Email Verification"];
+const formPages = ["Log in", "Sign Up form 1", "Sign Up form 2", "Sign up form 3", "Email Verification", "Account Successfully Created"];
 let currentPage = formPages[0];
 
 //SIGN UP BUTTON
-signupBtn.addEventListener("click", function() {
-    if(currentPage == formPages[0]) {
+signupBtn.addEventListener("click", function () {
+    if (currentPage == formPages[0]) {
         currentPage = formPages[1];
 
         cardContainer.className = "col-6";
         bodyOfLogin.style.display = "none";
+        credentialsForm1.style.display = "block";
         bodyOfSignup.style.display = "block";
+        
         pageTitle.innerText = "Sign Up";
     }
 });
 
 //SIGN UP BACK BUTTON AND NEXT
-backBtn.addEventListener('click',function() {
-    if(currentPage == formPages[1]) {
+backBtn.addEventListener('click', function () {
+    if (currentPage == formPages[1]) {
         currentPage = formPages[0];
 
         cardContainer.className = "col-4";
@@ -24,46 +26,82 @@ backBtn.addEventListener('click',function() {
         pageTitle.innerText = "Log In";
         cardContainer.style.height = "460px";
     }
-    else if(currentPage == formPages[2]) {
+    else if (currentPage == formPages[2]) {
         currentPage = formPages[1];
 
-        credentialsForm1.style.display = "block";
         credentialsForm2.style.display = "none";
+        credentialsForm1.style.display = "block";
     }
-    
+    else if (currentPage == formPages[3]) {
+        currentPage = formPages[2];
+
+        credentialsForm3.style.display = "none";
+        credentialsForm2.style.display = "flex";
+    }
+
 });
 
 //NEXT BUTTON 
-nextBtn.addEventListener('click', function() {
-    if(currentPage == formPages[1]) {
+nextBtn.addEventListener('click', function () {
+    if (currentPage == formPages[1]) {
         currentPage = formPages[2];
 
         credentialsForm1.style.display = "none";
         credentialsForm2.style.display = "flex";
     }
-    else if(currentPage == formPages[2]) {
+    else if (currentPage == formPages[2]) {
         currentPage = formPages[3];
+
+        credentialsForm2.style.display = "none";
+        credentialsForm3.style.display = "flex";
+    }
+    else if (currentPage == formPages[3]) {
+        currentPage = formPages[4];
 
         cardContainer.style.height = "381px";
         signupTitleTxt.innerText = "Enter Verification Code";
-        credentialsForm2.style.display = "none";
+        credentialsForm3.style.display = "none";
         verificationContainer.style.display = "block";
-        nextBtn.style.display = "none";
-        backBtn.innerText = "Verify";
+        backBtn.style.display = "none";
+        nextBtn.innerText = "Verify";
         buttonsContainer.style.gap = "0";
-        backBtn.classList.add("verifyBtn");
+        nextBtn.classList.add("verifyBtn");
+    }
+
+    else if (currentPage == formPages[4]) {
+        currentPage = formPages[5];
+
+        cardContainer.style.height = "120px";
+        accountSuccess.style.display = "flex";
+        bodyOfSignup.style.display = "none";
+
+        setTimeout(function () {
+            currentPage = formPages[0];
+
+            accountSuccess.style.display = "none";
+            cardContainer.className = "col-4";
+            bodyOfLogin.style.display = "block";
+            pageTitle.innerText = "Log In";
+            cardContainer.style.height = "460px";
+            verificationContainer.style.display = "none";
+            backBtn.style.display = "block";
+            nextBtn.innerText = "Next";
+            buttonsContainer.style.gap = "123px";
+            nextBtn.classList.remove("verifyBtn");
+            signupTitleTxt.innerText = "Sign Up";
+        }, 3500);
     }
 });
 
-incorrectEmailTxt.addEventListener('click', function() {
-    currentPage = formPages[2];
+incorrectEmailTxt.addEventListener('click', function () {
+    currentPage = formPages[3];
 
-        cardContainer.style.height = "434.014px";
-        signupTitleTxt.innerText = "Sign Up";
-        credentialsForm2.style.display = "flex";
-        verificationContainer.style.display = "none";
-        nextBtn.style.display = "block";
-        backBtn.innerText = "Back";
-        buttonsContainer.style.gap = "123px";
-        backBtn.classList.remove("verifyBtn");
+    cardContainer.style.height = "434.014px";
+    signupTitleTxt.innerText = "Sign Up";
+    credentialsForm3.style.display = "flex";
+    verificationContainer.style.display = "none";
+    backBtn.style.display = "block";
+    nextBtn.innerText = "Next";
+    buttonsContainer.style.gap = "123px";
+    nextBtn.classList.remove("verifyBtn");
 });
